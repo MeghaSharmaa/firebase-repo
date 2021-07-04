bc55a54166a28c3da71c2f2ee21b78d62c525246
MeghaSharmaa <77321862+MeghaSharmaa@users.noreply.github.com>
2021-07-04 23:08:30 +0530

    finalised

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const Config = {
-    apiKey: "AIzaSyAgiNQs95eL_9BJxTWqmVIhFEZtl1QxT0E",
-    authDomain: "know-it-all-9887d.firebaseapp.com",
-    databaseURL: "https://know-it-all-9887d-default-rtdb.firebaseio.com",
-    projectId: "know-it-all-9887d",
-    storageBucket: "know-it-all-9887d.appspot.com",
-    messagingSenderId: "394630709378",
-    appId: "1:394630709378:web:4a06052c8381829faa749b",
-    measurementId: "G-H5KZ970ETM"
+    apiKey: "AIzaSyC1kVMsjpUilSC-othxyvFKOZwZov170LY",
+    authDomain: "psychiz.firebaseapp.com",
+    databaseURL: "https://psychiz-default-rtdb.firebaseio.com",
+    projectId: "psychiz",
+    storageBucket: "psychiz.appspot.com",
+    messagingSenderId: "339029309611",
+    appId: "1:339029309611:web:ff1053ea7964c59c8a5fee",
+    measurementId: "G-HF9FWCZC2N"
 };
 
 
